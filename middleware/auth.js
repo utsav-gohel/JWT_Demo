6188631e25d843bba9be0a6d350abfaee41fc587
utsavgohel-simform <utsav@simformsolutions.com>
2023-03-20 17:47:14 +0530

    feat: implement middleware to check authorization

---
+++ middleware/auth.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+const SECRET_KEY = "NOTESAPI";
+
+const auth = (req, res, next) => {
+  try {
+    let token = req.headers.authorization;
+    if (token) {
+      token = token.split(" ")[1];
+      let user = jwt.verify(token, SECRET_KEY);
+      req.userId = user.id;
+    } else {
+      res.status(401).json({ msg: "unauthorized user" });
+    }
+    next();
+  } catch (e) {
+    res.status(401).json({ msg: "unauthorized user" });
+  }
+};
+
+module.exports = auth;
